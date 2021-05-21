2f8af6a20823fc2c18a46f30c5813a616f4ddd93
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-05-21 17:11:25 +0530

    completedJS

--- count.js
+++ count.js
@@ -1,20 +1,27 @@
-let a,b = 0
-$(document).ready(()=>{
-    
-})
+let a = 0
+let b = 0
+
+function init(){
+        a = Number($('#firstnumber').val())
+        b = Number($('#secondnumber').val())
+}
 
 function add(){
-    
+    init()
+    $('#result').text(a+b)
 }
 
 function sub(){
-    
+    init()
+    $('#result').text(a-b)
 }
 
 function mul(){
-    
+    init()
+    $('#result').text(a*b)
 }
 
-function mod(){
-    
+function div(){
+    init()
+    $('#result').text(a%b)
 }
